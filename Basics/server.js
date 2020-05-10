e3c764d03acf350c29de4d9e5ee2ce5eef8c5a4a
vishnu777aero <vishnu777aero@gmail.com>
2020-05-10 17:13:33 +0530

    Add what next end-point

--- Basics/server.js
+++ Basics/server.js
@@ -1,11 +1,21 @@
 const http = require('http');
 const PORT = 3000;
 
-
 const server = http.createServer( (req, res) => {
     if (req.url === '/') {
-        res.write('<h2>Hello world</h2>');
+        res.write('Hello world');
         res.end();
+    }
+
+    if (req.url === '/next') {
+        const next = [
+            'learn node advanced',
+            'learn express',
+            'learn ..........'
+        ];
+
+        res.write(JSON.stringify(next));
+        res.end();        
     }
 });
 
