e14b8dfe278005bedc02ae6008941a7f329e81e2
vishnu777aero <vishnu777aero@gmail.com>
2020-06-21 12:15:58 +0530

    First response

--- Maxmillian/3_basics/app.js
+++ Maxmillian/3_basics/app.js
@@ -7,8 +7,13 @@ const http = require('http');
 
 //rqListener will run for every incoming request
 const rqListener = (req, res) => {
-    console.log(req);
     //process.exit(); only if you want to shut the server :(, exits the event loop.
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+        res.write('<head><title>Hello from Node.js :)</title></head>');
+        res.write('<body><h1>Hello World!!</h2></body>');
+    res.write('</html>');
+    res.end();
 };
 
 const server = http.createServer(rqListener);
