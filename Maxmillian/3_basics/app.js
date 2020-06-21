b0e91b3c385f600e062c9983b49a282491e7de5b
vishnu777aero <vishnu777aero@gmail.com>
2020-06-21 11:50:40 +0530

    process.exit()

--- Maxmillian/3_basics/app.js
+++ Maxmillian/3_basics/app.js
@@ -8,6 +8,7 @@ const http = require('http');
 //rqListener will run for every incoming request
 const rqListener = (req, res) => {
     console.log(req);
+    //process.exit(); only if you want to shut the server :(, exits the event loop.
 };
 
 const server = http.createServer(rqListener);
