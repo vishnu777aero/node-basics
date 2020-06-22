524eea1e6aeeb498798c1aac76d6103a9e26b3ce
vishnu777aero <vishnu777aero@gmail.com>
2020-06-22 21:08:56 +0530

    Handle routes

--- Maxmillian/3_basics/Assignment/app.js
+++ Maxmillian/3_basics/Assignment/app.js
@@ -2,7 +2,34 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer( (req, res) => {
-    console.log('we doing good :)');
+    const { url } = req;
+
+    switch (url) {
+        case '/':
+            res.write('<h1>Hello world bug!</h1>');
+            res.end();
+            break;
+        case '/users':
+            res.write('<html>');
+                res.write('<body>');
+                    res.write('<ul>');
+                        res.write('<li>');
+                            res.write('Me :)')
+                        res.write('</li>');
+                        res.write('<li>');
+                            res.write('You :)')
+                        res.write('</li>');
+                        res.write('<li>');
+                            res.write('Everyone :)')
+                        res.write('</li>');
+                        res.write('<li>');
+                            res.write('Ill use template next time hahaha :)')
+                        res.write('</li>');
+                    res.write('</ul>');
+                res.write('</body>');
+            res.write('</html>');
+            res.end();
+    }
 });
 
 server.listen(PORT);
