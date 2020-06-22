1f2d8d7488fa6bfb6b3bda6e81aee6bd459a6b3e
vishnu777aero <vishnu777aero@gmail.com>
2020-06-23 00:31:58 +0530

    log request body message to console

--- Maxmillian/3_basics/Assignment/app.js
+++ Maxmillian/3_basics/Assignment/app.js
@@ -6,9 +6,29 @@ const server = http.createServer( (req, res) => {
 
     switch (url) {
         case '/':
-            res.write('<h1>Hello world bug!</h1>');
+            res.write('<html>');
+                res.write('<body>');
+                    res.write('<h1>Hello world bug!</h1>');
+                        res.write(`<form method='post' action='/create-user'>`);
+                            res.write(`<input name='heWhoMustNotBeNamed'>`);
+                            res.write(`<button type='submit'>click me!</button>`);
+                    res.write('</form>');
+                res.write('</body>');
+            res.write('</html>');
             res.end();
             break;
+        case '/create-user':
+            const body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+                console.log(chunk);
+            });
+            req.on('end', () => {
+                const parsedBody = Buffer.concat(body).toString();
+                const message = parsedBody.split('=')[1];
+                console.log(message);
+            });
+            return res.end();
         case '/users':
             res.write('<html>');
                 res.write('<body>');
