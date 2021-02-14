e6a77f86cc32f5661b340fd24640e8a55010bff1
vishnu777aero <vishnu777aero@gmail.com>
2021-02-14 14:05:53 +0530

    handle first response

--- relearn/first-server/app.js
+++ relearn/first-server/app.js
@@ -2,7 +2,12 @@ const http = require('http');
 
 //called for every incoming request!
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from Node.js</h1></body>')
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
