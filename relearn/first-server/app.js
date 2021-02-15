5a8c6d4b49e2ccbc23648bcad9d33c9e9ee6b320
vishnu777aero <vishnu777aero@gmail.com>
2021-02-15 23:59:12 +0530

    add form url

--- relearn/first-server/app.js
+++ relearn/first-server/app.js
@@ -2,6 +2,23 @@ const http = require('http');
 
 //called for every incoming request!
 const server = http.createServer((req, res) => {
+const { url } = req;
+
+if (url === '/form') {
+  res.write('<html>');
+  res.write('<head><title>My Second Page</title></head>');
+  res.write('<body>')
+  res.write('<h1>test form</h1>');
+  res.write('<form method="POST" action="/message">');
+  res.write('<input type="text" name="form-input" />');
+  res.write('<button type="submit">Submit</button>')
+  res.write('</from>');
+  res.write('</body>')
+  res.write('</html>');
+  return res.end();
+}
+
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
