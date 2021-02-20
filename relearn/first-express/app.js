d81798dba6f113e65b5f9eda7d9698f50d1dc132
vishnu777aero <vishnu777aero@gmail.com>
2021-02-21 01:53:19 +0530

    basic middleware

--- relearn/first-express/app.js
+++ relearn/first-express/app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('first middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('second middleware');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
