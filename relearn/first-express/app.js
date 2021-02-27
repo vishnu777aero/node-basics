c49f08528e2972d9ca82b580ae0fe5a003a29a51
vishnu777aero <vishnu777aero@gmail.com>
2021-02-27 20:38:02 +0530

    change url encoded extended to true

--- relearn/first-express/app.js
+++ relearn/first-express/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/add-product', (req, res) => {
   res.send('<form action="/post-product" method="POST"><input type="text" name="product" /><button type="submit">add</button></form>')
